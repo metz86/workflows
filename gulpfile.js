b5a69927923e34ecc3f8b8c88e376bab73ad9fd4
Mads <mads.are.martinsen@gmail.com>
2016-09-09 10:56:37 +0200

    Concatenated javascript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,24 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var coffee = require ('gulp-coffee');
+var concat = require('gulp-concat');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
+var jsSources = [
+    'components/scripts/rclick.js',
+    'components/scripts/pixgrid.js',
+    'components/scripts/tagline.js',
+    'components/scripts/template.js'];
 
 gulp.task('coffee', function() {
-    gulp.src(coffeeSources)
-        .pipe(coffee({bare: true})
+    gulp.src(coffeeSources) //specify source files
+        .pipe(coffee({bare: true}) //specify what to do
             .on('error', gutil.log))
-        .pipe(gulp.dest('components/scripts'))
-});      
+        .pipe(gulp.dest('components/scripts')) //specify destination folder
+});
+
+gulp.task('js', function() {
+    gulp.src(jsSources) //specify source files
+        .pipe(concat('script.js')) //specify what to do
+        .pipe(gulp.dest('builds/development/js')) //specify destination folder
+});
