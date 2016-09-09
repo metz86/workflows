1b46b5c6318e2603ae7483b1615dacf19c5b49b3
Mads <mads.are.martinsen@gmail.com>
2016-09-09 11:24:29 +0200

    Created default task

--- gulpfile.js
+++ gulpfile.js
@@ -39,3 +39,5 @@ gulp.task('compass', function() {
             .on('error', gutil.log))
         .pipe(gulp.dest('builds/development/css')) //specify destination folder
 });
+
+gulp.task('default', ['coffee', 'js', 'compass']);
